5d4ccffdede0adabc00bfbfde46a631fd594dfb9
gururaj <gururaj333415@gmail.com>
2024-07-19 10:44:07 +0530

    push

---
+++ src/test/java/com/example/SimpletestTest.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SimpletestTest {
+    @Test
+    public void multest() {
+        simpletest st = new simpletest();
+        assertEquals(25, st.multiple(5, 5));
+    }
+
+    @Test
+    public void divtest() {
+        simpletest st = new simpletest();
+        assertEquals(5.0, st.divide(25.0, 5.0), 0.1);
+    }
+
+    @Test
+    public void divzerotest() {
+        simpletest st = new simpletest();
+        assertEquals(0, st.divide(25.0, 0), 0.1);
+    }
+
+    @Test
+    public void facttest() {
+        simpletest st = new simpletest();
+        assertEquals(120, st.factorial(5));
+    }
+}
